590dd4f9cabfb90655904270695613af87895ef6
Ralphus <35075235+hu-ry@users.noreply.github.com>
2020-07-18 17:58:44 +0200

    Implemented noise_zigzag() of ScrollingPattern attribute
    - Added forward declaration of noise_zigzag_legacy() in noisedef.h
        - Current implementation of noise_zigzag() is going to be moved there in the future
    
    Signed-off-by: Ralphus <35075235+hu-ry@users.noreply.github.com>

--- src/model/noiseattribute.cpp
+++ src/model/noiseattribute.cpp
@@ -10,14 +10,13 @@
 namespace tasty {
 
 #define DELETION_INTERVAL 5U
-
+#define SATISFACTION_LIMIT 150
 
 
     NoiseAttribute noise_scrolling(Flavour &flavour, int x, int y) {
         return flavour.generateNoise(x, y);
     }
 
-    bool att_raining_isInit = false;
     int att_raining_noise_pop[CHAR_AMOUNT_PER_LINE] = { };
     int att_raining_current_y = -1;
     unsigned int att_raining_curr_noise_count = 0;
@@ -59,8 +58,58 @@ namespace tasty {
         //}
     }
 
+    int att_zigzag_current_y = -1;
+    int att_zigzag_amp = 4;
+
+    // 0 : if current pop is enabled with either 0 if not and 1 if active
+    // 1 : current amplitude offset state from owned pop
+    // 2 : current amplitude direction
+    // 3 : Satisfactory termination counter of owned pop
+    int att_zigzag_noise_pop[CHAR_AMOUNT_PER_LINE][4] = { };
+
     NoiseAttribute noise_zigzag(Flavour &flavour, int x, int y) {
-        return .5;
+
+        if(att_zigzag_current_y != y) {
+            att_zigzag_current_y = y;
+
+            for(int i=0; i < CHAR_AMOUNT_PER_LINE; i++) {
+                att_zigzag_noise_pop[i][2] =
+                        ((att_zigzag_noise_pop[i][1] == att_zigzag_amp) * -2)
+                        + (att_zigzag_noise_pop[i][2] * (att_zigzag_noise_pop[i][3]<SATISFACTION_LIMIT))
+                        + (2 * (att_zigzag_noise_pop[i][1] == -att_zigzag_amp));
+
+                att_zigzag_noise_pop[i][1] += att_zigzag_noise_pop[i][2];
+                att_zigzag_noise_pop[i][3] += (att_zigzag_noise_pop[i][0] == 1 && att_zigzag_noise_pop[i][3]<SATISFACTION_LIMIT)*1;
+            }
+
+            int newPopIndex = (int)random()%CHAR_AMOUNT_PER_LINE;
+
+            bool isInit = att_zigzag_noise_pop[newPopIndex][0] == 1;
+
+            att_zigzag_noise_pop[newPopIndex][1] = isInit * att_zigzag_noise_pop[newPopIndex][1];
+            att_zigzag_noise_pop[newPopIndex][2] =
+                    isInit * att_zigzag_noise_pop[newPopIndex][2]
+                    +
+                    (!isInit) * ((newPopIndex%2 == 0) * -2 + 1);
+            att_zigzag_noise_pop[newPopIndex][0] = 1;
+        }
+
+        int index = x % CHAR_AMOUNT_PER_LINE;
+
+        if((att_zigzag_noise_pop[index][0] == 1 && att_zigzag_noise_pop[index][1] == 0) ||
+        (att_zigzag_noise_pop[(index+1) % CHAR_AMOUNT_PER_LINE][0] == 1 && att_zigzag_noise_pop[index+1][1] == -1 ) ||
+                (att_zigzag_noise_pop[(index+2) % CHAR_AMOUNT_PER_LINE][0] == 1 && att_zigzag_noise_pop[index+2][1] == -2 ) ||
+                (att_zigzag_noise_pop[(index+3) % CHAR_AMOUNT_PER_LINE][0] == 1 && att_zigzag_noise_pop[index+3][1] == -3 ) ||
+                (att_zigzag_noise_pop[(index+4) % CHAR_AMOUNT_PER_LINE][0] == 1 && att_zigzag_noise_pop[index+4][1] == -4 ) ||
+                (att_zigzag_noise_pop[((index-1<0)*CHAR_AMOUNT_PER_LINE)+index-1][0] == 1 && att_zigzag_noise_pop[((index-1<0)*CHAR_AMOUNT_PER_LINE)+index-1][1] == 1 ) ||
+                (att_zigzag_noise_pop[((index-2<0)*CHAR_AMOUNT_PER_LINE)+index-2][0] == 1 && att_zigzag_noise_pop[((index-2<0)*CHAR_AMOUNT_PER_LINE)+index-2][1] == 2 ) ||
+                (att_zigzag_noise_pop[((index-3<0)*CHAR_AMOUNT_PER_LINE)+index-3][0] == 1 && att_zigzag_noise_pop[((index-3<0)*CHAR_AMOUNT_PER_LINE)+index-3][1] == 3 ) ||
+                (att_zigzag_noise_pop[((index-4<0)*CHAR_AMOUNT_PER_LINE)+index-4][0] == 1 && att_zigzag_noise_pop[((index-4<0)*CHAR_AMOUNT_PER_LINE)+index-4][1] == 4 )
+        ) {
+            return flavour.generateNoise(x, y);
+        } else {
+            return 0.0;
+        }
     }
 
     NoiseAttribute noise_static(Flavour &flavour, int x, int y) {
