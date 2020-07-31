6553fb90a0447057de3978b07efdb38dd352eebb
Ralphus <35075235+hu-ry@users.noreply.github.com>
2020-08-01 01:16:44 +0200

    Added scene.h for use in new frame operation mode
    Signed-off-by: Ralphus <35075235+hu-ry@users.noreply.github.com>

---
+++ include/model/scene/scene.h
@@ -0,0 +1,24 @@
+//##############################################################################
+//## Project: ClAni ########################### Created by hury on 01.08.2020 ##
+//##############################################################################
+//## Author: Ryan Huth(hury) ############################# License: GNU GPLv3 ##
+//##############################################################################
+
+#ifndef CLIANIMATION_SCENE_H
+#define CLIANIMATION_SCENE_H
+
+namespace tasty {
+
+    class Scene {
+    public:
+
+        virtual ~Scene() = default;
+
+    private:
+
+
+    };
+
+}; // end of namespace tasty
+
+#endif //CLIANIMATION_SCENE_H
