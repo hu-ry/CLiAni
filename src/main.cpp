14bd726b43d359e917944aeed7b09024853189b1
hury <35075235+hu-ry@users.noreply.github.com>
2024-03-02 03:21:21 +0100

    Fixes Segfault after exiting Ctrl+C
    - Temporary solution until I have found a proper way and probably after restructuring the program a bit
    
    Signed-off-by: hury <35075235+hu-ry@users.noreply.github.com>

--- src/main.cpp
+++ src/main.cpp
@@ -12,11 +12,11 @@
 #include <utilz/hutime.h>
 
 std::atomic<bool> quit(false);
-CliAniHury::Widdershins ctrl;
+CliAniHury::Widdershins* ctrl;
 
 void got_signal(int signum) {
     if (signum == SIGWINCH)
-        ctrl.scheduleResize();
+        ctrl->scheduleResize();
     else if (signum == SIGINT)
         quit.store(true);
 }
@@ -34,13 +34,14 @@ void got_signal(int signum) {
         sigaction(SIGWINCH, &sigInt, NULL);
 
         CliAniHury::CliView hr_view;
+        ctrl = new CliAniHury::Widdershins();
 
         // Registers View-module at ctrl for ncurses usage
-        ctrl.registerCliView(&hr_view);
+        ctrl->registerCliView(&hr_view);
         // Update the window size once in the beginning to fit the terminal
-        ctrl.scheduleResize();
+        ctrl->scheduleResize();
         // Starts program with requesting user input and preparing modules
-        ctrl.start();
+        ctrl->start();
 
         // Timer for getting deltaTime between iterations
         CliAniHury::HuTime curTime;
@@ -52,10 +53,10 @@ void got_signal(int signum) {
             lastTime = currentTime;
 
             // runs a "frame"
-            ctrl.runFrame(deltaTime);
+            ctrl->runFrame(deltaTime);
 
         }
-        ctrl.exit();
+        ctrl->exit();
 
         exit(SIGINT);
     }
