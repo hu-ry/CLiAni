0712d07b46a48d551cb7257c44b2bb1a31ca4e8b
hury <35075235+hu-ry@users.noreply.github.com>
2024-07-04 13:57:51 +0200

    Concurrent Raytracer data structure
    Signed-off-by: hury <35075235+hu-ry@users.noreply.github.com>

--- include/utilz/raytracer-threadsafe.h
+++ include/utilz/raytracer-threadsafe.h
@@ -7,8 +7,26 @@
 #ifndef CLIANIMATION_RAYTRACER_THREADSAFE_H
 #define CLIANIMATION_RAYTRACER_THREADSAFE_H
 
+#include <raytracer.h>
+
 class ThreadsafeRaytracer {
 public:
+struct ConstantGlobalData {
+    size_t m_MeshCount = 0;
+    std::vector<Raytracer::Mesh> m_Meshes;
+    uint32_t m_WindowHeight;
+    uint32_t m_WindowWidth;
+};
+
+struct GlobalData {
+    std::shared_ptr<tasty::Camera> m_Camera;
+    std::vector<humath::v3f> m_RayAngles;
+
+};
+
+
+private:
+
 
 
 };
